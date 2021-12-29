3d6fad3a8608e650913fb4ad6b301af6af3e28d1
Kasper <kasper.gaworski@wp.pl>
2021-12-29 14:49:46 +0100

    Svelte fetching and card component

--- frontend/src/App.svelte
+++ frontend/src/App.svelte
@@ -1,11 +1,41 @@
 <script>
-	export let name;
+	import { onMount } from "svelte";
+	import Card from './components/Card.svelte'
+
+	let name = ':)';
+	let posts = [];
+	onMount(async () => {
+		const response = await fetch('http://localhost:8000/api/posts');
+		const data = await response.json();
+		console.log(data.posts);
+		posts = data.posts;
+	});
 </script>
 
+
+
 <main>
-	<h1>Hello {name}!</h1>
+	<h1>Hello {name} !</h1>
 	<p>Visit the <a href="https://svelte.dev/tutorial">Svelte tutorial</a> to learn how to build Svelte apps.</p>
+	
+	{#each posts as article}
+	<div>
+		<Card>
+			<div class="title">
+				<h3>{article.title}</h3>
+			</div>
+			<div class="author">
+				<h4>Author: {article.author.name}</h4>
+			</div>
+			<div class="content">
+				<p>{article.body}</p>
+			</div>
+		</Card>
+	</div>
+	{/each}
 </main>
+
+
 
 <style>
 	main {
@@ -26,5 +56,23 @@
 		main {
 			max-width: none;
 		}
+	}
+
+
+	/* Styling card contents */
+	.title{
+		color: black;
+		font-weight: bold;
+		margin: 0.5em
+	}
+	.author{
+		text-align: left;
+		color: #555;
+		font-weight: 200;
+		margin: 0.5em;
+	}
+	.content{
+		margin: 0.5em;
+		text-align: left;
 	}
 </style>
---
+++ frontend/src/components/Card.svelte
@@ -0,0 +1,19 @@
+<script>
+
+</script>
+
+<div class="card container">
+    <div class="card-content">
+    <slot></slot>
+    </div>
+</div>
+
+<style>
+    .card{
+        background-color: white;
+        padding: 1.5em 3em;
+        border-radius: 20px;
+        box-shadow: 3px 3px #555;
+    }
+
+</style>
