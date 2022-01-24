113db11d8043b5ccf1e57fd8c6b4573f26e68b05
Szlendakooo <95409714+Szlendakooo@users.noreply.github.com>
2022-01-24 12:07:34 +0100

    szlendak-proba polaczenia

--- frontend/src/App.svelte
+++ frontend/src/App.svelte
@@ -3,7 +3,10 @@
 	import Card from './components/Card.svelte'
 	import { apiData, drinkNames} from './store.js';
 
-	let name = ':)';
+	let text1 = 'Dane Organizacji Narodów Zjednoczonych do spraw Wyżywienia i Rolnictwa (FAO), wskazują, że na świecie marnujemy rocznie ok. 1,3 miliarda ton jedzenia, co stanowi ok. 1/3 produkowanej żywności. W Polsce w koszu ląduje jej ok. 9 mln ton.'
+	let text2 = 'Masz za dużo jedzenia a nie chcesz go wyrzucać? Podaruj go innym i przyczyń się do lepszego jutra! Nie masz co dziś zjeść na obiad? Zarezerwuj darmowy posiłek i powstrzymaj marnowanie się jedzenia!'
+	let text3 = 'Jest to dziecinnie proste! Utwórz ogłoszenie o swoim jedzeniu, załącz zdjęcie, opis oraz adres odbioru, a druga osoba w przeciągu 15 minut odbierze to, na co nie masz już ochoty!Szukasz jedzenia? Wybierz interesujący Cię posiłek i odbierz go w przeciągu 15 minut. Smacznego!'
+	let name = 'FOODO';
 	let articles = [];
 	let orders = [];
 	let address = [];
@@ -69,19 +72,56 @@
 
 
 <main>
-	<h1>Hello {name} !</h1>
-	<p>Visit the <a href="https://svelte.dev/tutorial">Svelte tutorial</a> to learn how to build Svelte apps.</p>
+	<div id="banner">
+		<h1>
+			{name}
+		</h1>
+		</div>	
+		
+		<div class="flex">
+			<article>
+				<div class="title_left">
+					<h2>{text1}</h2>
+				<div>
+			</article>
 	
-	
->  <a class="skiplink" href="#map">Go to map</a>
-<div id="map" class="map" tabindex="0"></div>
-<div id="popup" class="ol-popup">
-	<div id="popup-content"></div>
-</div>
-<button id="zoom-out">Oddal</button>
-<button id="zoom-in">Przybliż</button>
-<script src="main.js"></script>
+			<article>
+				<div class="col">
+					<a class="skiplink" href="#map">Go to map</a>
+							<div id="map" class="map" tabindex="0"></div>
+							<div id="popup" class="ol-popup">
+							<div id="popup-content"></div>
+							</div>
+							<button id="zoom-out">Oddal</button>
+							<button id="zoom-in">Przybliż</button>
+							<script src="main.js"></script>
 
+				</div>
+			</article>
+	
+			<article>
+				<div class="title_right">
+					<h3>{text2}</h3>
+				<div>
+			</article>
+		</div>
+	
+		<button>
+			test
+		</button>
+	
+	
+		<div>
+			<article>
+				<div class="title">
+					<h4>{text3}</h4>
+				<div>
+			</article>
+		</div>
+	
+	
+	
+>  
 
 
 	
@@ -136,45 +176,86 @@
 <style>
 	main {
 		text-align: center;
-		padding: 1em;
-		max-width: 240px;
-		margin: 0 auto;
+	}
+	
+	button{
+		background-color: black;
+		color: white;
+		width:100px;
 	}
 
-	h1 {
-		color: #ff3e00;
-		text-transform: uppercase;
-		font-size: 4em;
-		font-weight: 100;
-	}
 
-	@media (min-width: 640px) {
-		main {
-			max-width: none;
-		}
+	div#banner { 
+       position: absolute; 
+       top: 0; 
+       left: 0; 
+       width: 100%; 
+	   height: 150px;
+	   background-color: #F26363;
+	   
+     }
+
+
+		h1 {
+		color: white;
+		background-color: #F26363;
+		font-size: 6em;
+		font-weight: 450;
 	}
+	
+
 
 
 	/* Styling card contents */
-	.title{
-		color: black;
+	.title_right{
+		color: white;
 		font-weight: bold;
-		margin: 0.5em
-	}
-	.author{
-		text-align: left;
-		color: #555;
-		font-weight: 200;
 		margin: 0.5em;
+		font-size:x-large;
+		line-height: 30px;
+		margin-left: auto;
+		margin-right: auto;
+		margin-top: 150px;
+		background-color: #F26363;
+        border-radius: 20px;
+		padding: 30px;
+		width:25%;
+		float: right;
 	}
-	.content{
+
+	.title_left{
+		color: white;
+		font-weight: bold;
 		margin: 0.5em;
-		text-align: left;
+		font-size:x-large;
+		line-height: 30px;
+		margin-left: auto;
+		margin-right: auto;
+		margin-top: 150px;
+		background-color: #F26363;
+        border-radius: 20px;
+		padding: 30px;
+		width:25%; 
+		float:left;
+	}
+
+	.flex{
+		display: flex;
+  		flex-wrap: nowrap;
+		width: 100%;
+	}
+	.col{
+		background-color: lightblue;
+		margin-top: 150px;
+        border-radius: 20px;
+		padding: 30px;
+		width: 400%;
+		margin-right: 50%;
 	}
 
 
 
-
+	
 	.map {
         width: 100%;
         height:400px;
